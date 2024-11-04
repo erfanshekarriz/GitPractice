3f6b1d4295e9892f06214fd9a4a4131b1eda34e8
erfanshekarriz <eshekarriz@connect.ust.hk>
2024-11-04 12:36:39 +0800

    transform_table.R now saves output as csv file

---
+++ scripts/transform_table.R
@@ -0,0 +1,11 @@
+library(dplyr)
+
+
+df <- data.frame(
+  x = rnorm(10), # Random normal values for column "x"
+  y = runif(10), # Random uniform values for column "y"
+  z = sample(letters, 10, replace = TRUE) # Random letters for column "z"
+)
+
+dfnew <- dplyr::mutate(df, J="K")
+write.csv(dfnew, "./data/tabular/newtable.csv")
