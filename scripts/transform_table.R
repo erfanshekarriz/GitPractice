fae52119d9619ac273cfc01aa47850d46d61a7c5
erfanshekarriz <eshekarriz@connect.ust.hk>
2024-11-04 14:11:26 +0800

    Added YYY column to transform_table.R

--- scripts/transform_table.R
+++ scripts/transform_table.R
@@ -7,5 +7,6 @@ df <- data.frame(
   z = sample(letters, 10, replace = TRUE) # Random letters for column "z"
 )
 
-dfnew <- dplyr::mutate(df, J="K", I="i", E="Erfan", O="Bernice")
+dfnew <- dplyr::mutate(df, J="K", I="i", E="Erfan", O="Bernice", 
+                       XXX="hello", YYY="whywhywhy")
 write.csv(dfnew, "./data/tabular/newtable.csv")
